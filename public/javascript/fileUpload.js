7776fdbcf6a8b3a944c3308bf718be52106c1058
efisir <efisir@gmail.com>
2022-10-03 17:04:35 +0300

    filepond image browse area dimentions bug fix

--- public/javascript/fileUpload.js
+++ public/javascript/fileUpload.js
@@ -8,6 +8,10 @@ if (rootStyles.getPropertyValue('--book-cover-large-width') != null
     .addEventListener('load', ready)
 }
 
+window.addEventListener("load", function () {
+    
+    window.setTimeout(runAfterLoad, 2000);
+}, false);
 function ready() {
 
         const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-large-width'))
@@ -20,12 +24,22 @@ function ready() {
         )
 
 
-
+        console.log('coverAspectRatio', coverAspectRatio, 'coverWidth', coverWidth, 'coverHeight', coverHeight )
         FilePond.parse(document.body)
         FilePond.setOptions({
             stylePanelAspectRatio: 1 / coverAspectRatio,
             imageResizeTargetWidth: coverWidth,
             imageResizeTargetHeight: coverHeight
         })
+
+
+}
+
+
+function runAfterLoad(){
+    const filePondDiv = document.getElementsByClassName('filepond--root')
+    console.log('before', filePondDiv)
+    filePondDiv.style = "color:red; width: 250; height: 375;"
+    console.log('after', filePondDiv)
 }
 
