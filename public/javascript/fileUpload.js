effeff6e9a2ee0083b0cf14cccb978e5770614fa
efisir <efisir@gmail.com>
2022-10-03 13:28:14 +0300

    finish app

--- public/javascript/fileUpload.js
+++ public/javascript/fileUpload.js
@@ -1,13 +1,31 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode    
-)
+const rootStyles = window.getComputedStyle(document.documentElement)
+
+if (rootStyles.getPropertyValue('--book-cover-large-width') != null 
+    && rootStyles.getPropertyValue('--book-cover-large-width')!== '') {
+    ready()
+}else{
+    document.getElementById('main-css')
+    .addEventListener('load', ready)
+}
+
+function ready() {
+
+        const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-large-width'))
+        const coverAspectRatio =  parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+        const coverHeight = coverWidth / coverAspectRatio
+        FilePond.registerPlugin(
+            FilePondPluginImagePreview,
+            FilePondPluginImageResize,
+            FilePondPluginFileEncode    
+        )
 
 
-FilePond.parse(document.body)
-FilePond.setOptions({
-    stylePanelAspectRatio: 150/100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+
+        FilePond.parse(document.body)
+        FilePond.setOptions({
+            stylePanelAspectRatio: 1 / coverAspectRatio,
+            imageResizeTargetWidth: coverWidth,
+            imageResizeTargetHeight: coverHeight
+        })
+}
+
